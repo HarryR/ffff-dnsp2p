34173aa4ffe0acbccd1b50a52a980e080fab6183
cjdelisle <calebdelisle@lavabit.com>
2010-12-15 00:37:34 -0500

    Wrote first actual test.

---
+++ test/dns_test.c
@@ -0,0 +1,19 @@
+#include "test_framework.c"
+#include "../dns.c"
+
+int count_char_test(void)
+{
+    const char *str = "abcabcabc1234554321";
+    int out = 0;
+    out += assertTrue((count_char(str, 'a') == 3), "Character count incorrect for char 'a'");
+    out += assertTrue((count_char(str, '1') == 2), "Character count incorrect for char '1'");
+    out += assertTrue((count_char(str, '5') == 2), "Character count incorrect for char '5'");
+    return out;
+}
+
+int main(void)
+{
+    int out = 0;
+    out += count_char_test();
+    return 0;
+}
--- test/f4_test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "../libbenc/benc.h"
-
-int main(void)
-{
-    printf("Hello World!\n");
-    return 0;
-}
